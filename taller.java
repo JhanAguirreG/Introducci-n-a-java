2ec8d9a30ca66f114e4fe1f5664ebcc7816d39ce
nosferatus115 <138174540+nosfetarus115@users.noreply.github.com>
2024-04-18 18:24:27 -0500

    taller...
    taller java sebastian

--- taller.java
+++ taller.java
@@ -4,6 +4,7 @@ public class taller {
     public static void main(String[] args) {
        int name;
        double radio=0;
+       int num=0;
         
        Scanner Scanner = new Scanner(System.in);
 
@@ -12,8 +13,22 @@ public class taller {
          System.out.print("perfecto, "+name );
          System.out.print(" ahora digita un radio que deses calcular");
          radio =Scanner.nextInt();
-         double area=Math.PI*Math.pow(radio, 2);
+            double area=Math.PI*Math.pow(radio, 2);
+              //___\\
+            System.out.print("el areae es: "+area);
+            //___________________________________________________________
+        if (area%2 ==0){
+            System.out.println(area+" Es divisible por 2");
+        }else{
+            System.out.println(area+" No es divisible por 2");
 
+        }
+      //__________________\\
+      System.out.println(name+"digita el caracter que desas ver en la tabla ASCII");
+      char caracter = sc.next().CharAT(0);
+      int codigo = (int) caracter;
+ 
+        System.out.println("ASCII: "+codigo);
 
 
     }
