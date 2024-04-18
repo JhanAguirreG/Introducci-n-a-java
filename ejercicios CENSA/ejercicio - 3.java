1742405b9be0e332cf7ce75014e6d0dcd6110aed
Juan Rondón <138329209+juanrondon05@users.noreply.github.com>
2024-04-18 18:28:31 -0500

    EJERCICIOS CENSA -

---
+++ ejercicios CENSA/ejercicio - 3.java
@@ -0,0 +1,16 @@
+public class BucleInfinito {
+ 
+    public static void main(String[] args) {
+ 
+        int num=1;
+ 
+        //Definimos el bucle, incluye el 100
+        while (num<=100){
+            System.out.println(num);
+            //Incrementamos num
+            num++;
+        }
+    }
+}
+
+
