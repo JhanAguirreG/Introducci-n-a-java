d1c7adbea0526f145f4e675420ceb629fa9fcc1b
TrevorStocker <138174051+TrevorStocker@users.noreply.github.com>
2024-04-18 17:55:20 -0500

    Add files via upload

---
+++ JavaExcersices/Asscii02.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+public class Asscii02 {
+ 
+    public static void main(String[] args) {
+ 
+         
+        String texto=JOptionPane.showInputDialog("Introduce un caracter ASCII");
+        //Pasamos el String a char con el metodo charAt
+        char caracter=texto.charAt(0);
+  
+        //Pasamos el caracter a codigo
+        int codigo=(int)caracter;
+  
+        System.out.println(codigo);
+    }
+}
+
+
+ 
---
+++ JavaExcersices/hola.java
@@ -0,0 +1,10 @@
+public class hola {
+ 
+    public static void main(String[] args) {
+ 
+        String nombre=" Doug Dimmadome, dueño del Domodín de Dimmsdale!";
+ 
+        System.out.println("Bienvenido "+nombre);
+    }
+}
+
