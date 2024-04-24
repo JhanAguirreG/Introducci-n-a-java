fa4ddcecfeac69f2d1b0e3b17cc22036b1ca1658
Juan Rondón <138329209+juanrondon05@users.noreply.github.com>
2024-04-23 19:44:15 -0500

    Update Ejercicio 1 -tablas.java

--- EJERCICIOS 2/Ejercicio 1 -tablas.java
+++ EJERCICIOS 2/Ejercicio 1 -tablas.java
@@ -1,10 +1,10 @@
 import javax.swing.JOptionPane;
-public class tablas {
+public class Tablasmultiplicar {
  
     public static void main(String[] args) {
         int i = 0;
-        String texto=JOptionPane.showInputDialog("Introduce el precio de un producto");
-        //Pasamos el String a double
+        String texto=JOptionPane.showInputDialog("Introduce un número");
+        
         int tablasmultiplos=Integer.parseInt(texto);
         
         while (i <= 10) {
