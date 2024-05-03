2a2512ddc835472736025fcf4110167502a56855
nosferatus115 <138174540+nosfetarus115@users.noreply.github.com>
2024-05-02 20:19:09 -0500

    Add files via upload

---
+++ Prueba_desempeño/ejercio4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ejercio4 {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("digita tu nombre");
+        String nombre = sc.nextLine();
+
+        
+            System.out.println("hola "+nombre+" ¿Como has estado?");
+        
+    
+    }   
+}
